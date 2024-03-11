444121739e2b957e61deed019152d0aac07e0eae
Yalshire2004 <eric.attere@un.org>
2024-03-11 12:27:16 -0400

    first commit to git

--- dl/static/js/vuejs/index.js
+++ dl/static/js/vuejs/index.js
@@ -2,13 +2,6 @@ Vue.component('index', {
     template: `
                 <div class="alert alert-secondary" role="alert">
                     <h2 class="alert-heading"> Dynamic Listings  </h2>
-                <hr>
-                <p>request for data : https://metadata.un.org/editor/api/marc/bibs/records/753214/fields/989/0<p/>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore <p/>
-                <p>et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip <p/>
-                <p>ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu <p/>
-                <p>fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt <p/>
-                <p>mollit anim id est laborum.</p>
                 </div>
                  `
   })
